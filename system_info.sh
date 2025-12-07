4e031200e1212f7a278d657eeaf43f19b1ffa50f
Arunponugoti <arun.devop1@gmail.com>
2025-12-07 12:12:52 +0530

    9th iteration

--- system_info.sh
+++ system_info.sh
@@ -8,9 +8,11 @@
 # It is intended for use in Linux environments.
 # Usage: ./system_info.sh
 
+echo "Collecting system information..."
+
 REPORT_FILE="system_info_report_$(date +%Y%m%d_%H%M%S).txt"
 
 echo "================================" > ${REPORT_FILE}
 echo "     system info report    " >> ${REPORT_FILE}
 echo "================================" >> ${REPORT_FILE}
-echo "" > ${REPORT_FILE}
+echo "" >> ${REPORT_FILE}
